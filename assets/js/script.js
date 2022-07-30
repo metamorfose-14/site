e15c6d08b8535fd1f0fdd53fbd89b5c665402a49
Céo <noah.kd@gmail.com>
2022-07-30 00:15:25 -0300

    OUGUGOI

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,4 +55,114 @@ setCookie('fbreplies',JSON.stringify(fbreplies),today)}}
 setTimeout(function(){$('#add-to-cart').show()},20000)
 var today=new Date()
 today.setDate(today.getFullYear()+1)
+
+/*  // 2. This code loads the IFrame Player API code asynchronously. */
+var tag = document.createElement('script');
+var sessaoStateChange = 0;
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+/*  // 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads. */
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'wB8YkcI2-50',
+    playerVars: {
+      'playsinline': 1,
+      'controls': 0,
+      'modestbranding': 1,
+      'autoplay': 1,
+      'rel': 0,
+      'disablekb': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+/* // 4. The API will call this function when the video player is ready. */
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+/*  // 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop. */
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+   /*  setTimeout(stopVideo, 6000); */
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+/* 
+function soltaOPlay() {
+  player.playVideo();
+} */
+
+function soltaOPlay() {
+  document.querySelector( ".ytp-large-play-button" ).classList.toggle( "disable-Btn" );
+  document.querySelector( ".ytp-large-pause-button" ).classList.toggle( "enable-Btn" );
+  player.playVideo();
+}
+
+function tacaOPause() {
+  document.querySelector( ".ytp-large-play-button" ).classList.toggle( "disable-Btn" );
+  document.querySelector( ".ytp-large-pause-button" ).classList.toggle( "enable-Btn" );
+  player.pauseVideo();
+}
+
+function sessaoDaTarde() {
+  /* // var sessaoStateChange; */
+  switch(sessaoStateChange) {
+    case 0:
+      /* // document.querySelector( ".ytp-large-play-button" ).classList.toggle( "disable-Btn" );
+      // document.querySelector( ".ytp-large-pause-button" ).classList.toggle( "enable-Btn" ); */
+      player.playVideo();
+      sessaoStateChange = 1;
+      document.querySelector( ".ytp-large-pause-button" ).classList.replace( "invisivel", "visivel" );
+      document.querySelector( ".ytp-large-play-button" ).classList.replace( "enable-Btn", "disable-Btn" );
+      document.querySelector( ".ytp-large-pause-button" ).classList.replace( "disable-Btn", "enable-Btn" );
+      setTimeout(paused, 800);
+      function paused() {
+        document.querySelector( ".ytp-large-pause-button" ).classList.replace( "visivel", "invisivel" );
+      }
+      /* // document.getElementsByClassName( ".ytp-large-play-button" ) */
+      break;
+    case 1:
+      /* // document.querySelector( ".ytp-large-play-button" ).classList.toggle( "disable-Btn" );
+      // document.querySelector( ".ytp-large-pause-button" ).classList.toggle( "enable-Btn" ); */
+      player.pauseVideo();
+      sessaoStateChange = 0;
+      document.querySelector( ".ytp-large-pause-button" ).classList.replace( "invisivel", "visivel" );
+      document.querySelector( ".ytp-large-pause-button" ).classList.replace( "enable-Btn", "disable-Btn" );
+      document.querySelector( ".ytp-large-play-button" ).classList.replace( "disable-Btn", "enable-Btn" );
+      break;
+  }
+}
+
+setTimeout(() => {
+    window.scrollTo(0,0);
+    document.querySelector(".headerbtn").style.display = "block";
+    document.querySelector(".cardb").style.display = "block";
+    document.querySelector(".cardb1").style.display = "block";
+    document.querySelector("#aboutme").style.display = "flex";
+    document.querySelector("#provasocial").style.display = "block";
+    document.querySelector("#provasocial1").style.display = "block";
+    document.querySelector("como-funciona").style.display = "block";
+    document.querySelector("bonus").style.display = "block";
+    document.querySelector("garantia").style.display = "block";
+    document.querySelector("#rodape-m14").style.display = "flex";
+    /* document.querySelector("comments").style.display = "none"; */
+  }, 1096200);
 // setCookie('returningVisitor','yes',today)
